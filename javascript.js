7743cc13d00b3b65d15ad0674264b3287e754ab5
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-02-14 01:13:54 -0800

    Feat: add addBookToLibrary function

--- javascript.js
+++ javascript.js
@@ -15,29 +15,35 @@ const duneMessiah = new Book("Dune Messiah", "Frank Herbert", 337, "not read");
 
 const myLibrary = [];
 
-function addBookToLibrary() {
-  const modal = document.getElementById('modal');
-  modal.showModal();
-
-/*   alert("it works");
- */}
-
-
-const addBookButton = document.getElementById('addBook');
-addBookButton.addEventListener('click', addBookToLibrary);
-
-const submitButton = document.getElementById('submit');
-/* submitButton.addEventListener('click', function(event) {event.preventDefault()});
- */submitButton.addEventListener('click', test);
-function test(event) {
-  event.preventDefault()
-}
-
 myLibrary.push(dune);
 myLibrary.push(duneMessiah);
 console.log(myLibrary);
 
-/* const form = document.getElementById('form');
- */let formData = new FormData(form);
 
-console.log(formData);
+function openModal() {
+  const modal = document.getElementById('modal');
+  modal.showModal();
+}
+
+const addBookButton = document.getElementById('addBook');
+addBookButton.addEventListener('click', openModal);
+
+
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', addBookToLibrary);
+
+function addBookToLibrary() {
+  let newBook = new Book(
+    title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    pages = document.getElementById('pages').value,
+/*     read = document.getElementById('read').value,
+ */  );
+  console.log(newBook);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  modal.close();
+  document.getElementById('title').value =''
+  document.getElementById('author').value =''
+  document.getElementById('pages').value =''
+}
