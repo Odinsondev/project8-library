dd5fe05e2edec6bd2e9a74f92e6a2d96ed21aa71
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-02-20 03:01:08 -0800

    Feat: add 'read' checkbox functionality
    Feat: add 'delete' button

--- javascript.js
+++ javascript.js
@@ -33,12 +33,20 @@ const submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', addBookToArray);
 
 function addBookToArray() {
+
+  let ifRead = '';   // read/not read checkbox
+  if (document.getElementById('read').checked === true) {
+    ifRead = 'read';
+  } else if (document.getElementById('read').checked === false) {
+    ifRead = 'not read';
+  } else {}
+
   let newBook = new Book(
     title = document.getElementById('title').value,
     author = document.getElementById('author').value,
     pages = document.getElementById('pages').value,
-/*     read = document.getElementById('read').value,
- */  );
+    read = ifRead
+  );
   console.log(newBook);
   myLibrary.push(newBook);
   console.log(myLibrary);
@@ -46,8 +54,8 @@ function addBookToArray() {
   document.getElementById('title').value =''
   document.getElementById('author').value =''
   document.getElementById('pages').value =''
+  document.getElementById('read').checked = false
   addBookToLibrary();
-  console.log(read);   //next to figure out
 }
 
 function addBookToLibrary() {
@@ -55,16 +63,29 @@ function addBookToLibrary() {
     let cellId1 = `cell${i}1`;
     let cellId2 = `cell${i}2`;
     let cellId3 = `cell${i}3`;
+    let cellId4 = `cell${i}4`;
+    let cellId5 = `cell${i}5`;
     let cell1 = document.getElementById(`${cellId1}`);
     let cell2 = document.getElementById(`${cellId2}`);
     let cell3 = document.getElementById(`${cellId3}`);
+    let cell4 = document.getElementById(`${cellId4}`);
+    let cell5 = document.getElementById(`${cellId5}`);
     cell1.textContent = myLibrary[i].title;
     cell2.textContent = myLibrary[i].author;
     cell3.textContent = myLibrary[i].pages;
+    cell4.textContent = myLibrary[i].read;
+
+    let test = document.getElementById(`button${i}`);   //to test if element exists
+
+    if (test === null) {   //testing if button already exists before creating new button
+      let newButton = document.createElement('button');
+      newButton.textContent = "Delete";
+      newButton.setAttribute('id', `button${i}`);
+      cell5.appendChild(newButton);
+    } else {}
   }
 }
 
 addBookToLibrary();
 
-let read = document.getElementById('read');   //next to figure out
-console.log(read);
+//make delete button work. note: number in button id is same than book array location
