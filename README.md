4e70f12977154910aea776ade399e973585a606a
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-05-30 19:22:47 -0700

    Chore: edit readme.md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # project8-library
+
 My eighth Odin project - Library
 
 I'm going to build a small Library app.
@@ -8,3 +9,6 @@ I'm going to use object constructors and prototypal inheritance.
 Revisiting project to refactor some javascript code.
 Javascript-ver2.js will use class instead of object constructor.
 Javascript-ver2.js will also use a class method.
+
+30.05.2024
+Revisiting the project to add form validation.
--- javascript.js
+++ javascript.js
@@ -1,28 +1,50 @@
 //javascript.js version 1 - using object constructor
 
-function Book(title, author, pages, read) {   //object constructor for Book
+//object constructor for Book
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-Book.prototype.toggleRead = function() {   //function to toggle read status
-  if (this.read === "Read") {
-    this.read = "Not read"
-  } else if (this.read === "Not read") {
-    this.read = "Read"
-  } else {}
-  addBookToLibrary();   //to update the list of books
-}
+//function to toggle read status
+Book.prototype.toggleRead = function () {
+  if (this.read === 'Read') {
+    this.read = 'Not read';
+  } else if (this.read === 'Not read') {
+    this.read = 'Read';
+  } else {
+  }
+  addBookToLibrary(); //to update the list of books
+};
 
-
-const dune = new Book("Dune", "Frank Herbert", 658, "Read");
-const duneMessiah = new Book("Dune Messiah", "Frank Herbert", 337, "Read");
-const childrenOfDune = new Book("Children of Dune", "Frank Herbert", 609, "Not read");
-const godEmperorOfDune = new Book("God Emperor of Dune", "Frank Herbert", 587, "Not read");
-const theHereticsOfDune = new Book("Heretics of Dune", "Frank Herbert", 669, "Not read");
-const chapterhouseDune = new Book("Chapterhouse: Dune", "Frank Herbert", 624, "Not read");
+const dune = new Book('Dune', 'Frank Herbert', 658, 'Read');
+const duneMessiah = new Book('Dune Messiah', 'Frank Herbert', 337, 'Read');
+const childrenOfDune = new Book(
+  'Children of Dune',
+  'Frank Herbert',
+  609,
+  'Not read'
+);
+const godEmperorOfDune = new Book(
+  'God Emperor of Dune',
+  'Frank Herbert',
+  587,
+  'Not read'
+);
+const theHereticsOfDune = new Book(
+  'Heretics of Dune',
+  'Frank Herbert',
+  669,
+  'Not read'
+);
+const chapterhouseDune = new Book(
+  'Chapterhouse: Dune',
+  'Frank Herbert',
+  624,
+  'Not read'
+);
 
 const myLibrary = [];
 
@@ -33,9 +55,7 @@ myLibrary.push(godEmperorOfDune);
 myLibrary.push(theHereticsOfDune);
 myLibrary.push(chapterhouseDune);
 
-
 console.log(myLibrary);
-
 
 function openModal() {
   const modal = document.getElementById('modal');
@@ -45,55 +65,53 @@ function openModal() {
 const addBookButton = document.getElementById('addBook');
 addBookButton.addEventListener('click', openModal);
 
-
 const cancelButton = document.getElementById('cancel');
 cancelButton.addEventListener('click', cancelModal);
 
 function cancelModal() {
   modal.close();
-  document.getElementById('title').value =''
-  document.getElementById('author').value =''
-  document.getElementById('pages').value =''
-  document.getElementById('read').checked = false
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
+  document.getElementById('read').checked = false;
 }
-
 
 const submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', addBookToArray);
 
 function addBookToArray() {
-
-  let ifRead = '';   // read/not read checkbox
+  let ifRead = ''; // read/not read checkbox
   if (document.getElementById('read').checked === true) {
     ifRead = 'Read';
   } else if (document.getElementById('read').checked === false) {
     ifRead = 'Not read';
-  } else {}
+  } else {
+  }
 
-  const title = document.getElementById('title').value;   // get data from form
+  const title = document.getElementById('title').value; // get data from form
   const author = document.getElementById('author').value;
   const pages = document.getElementById('pages').value;
   const read = ifRead;
 
-  const newBook = new Book(`${title}`,`${author}`, `${pages}`, `${read}`);   //create Book object
+  const newBook = new Book(`${title}`, `${author}`, `${pages}`, `${read}`); //create Book object
 
   console.log(newBook);
-  myLibrary.push(newBook);   //add new Book object to Array
+  myLibrary.push(newBook); //add new Book object to Array
   console.log(myLibrary);
   modal.close();
-  document.getElementById('title').value =''   //delete form entries when closing modal
-  document.getElementById('author').value =''
-  document.getElementById('pages').value =''
-  document.getElementById('read').checked = false
+  document.getElementById('title').value = ''; //delete form entries when closing modal
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
+  document.getElementById('read').checked = false;
   addBookToLibrary();
 }
 
+//display Array objects on page
+function addBookToLibrary() {
+  clearCells(); //clear everything when adding or deleting a book
 
-function addBookToLibrary() {   //display Array objects on page
-
-  clearCells();   //clear everything when adding or deleting a book
-
-  for(let i = 0; i <= myLibrary.length - 1; i++) {   //loop through myLibrary Array
+  //loop through myLibrary Array
+  for (let i = 0; i <= myLibrary.length - 1; i++) {
     const cell1 = document.getElementById(`cell${i}1`);
     const cell2 = document.getElementById(`cell${i}2`);
     const cell3 = document.getElementById(`cell${i}3`);
@@ -104,39 +122,40 @@ function addBookToLibrary() {   //display Array objects on page
     cell3.textContent = myLibrary[i].pages;
     cell4.textContent = myLibrary[i].read;
 
-    const deleteButtonTest = document.getElementById(`button${i}`);   //test if element exists
+    const deleteButtonTest = document.getElementById(`button${i}`); //test if element exists
 
-    if (deleteButtonTest === null) {   //testing if button already exists before creating new button
+    //testing if button already exists before creating new button
+    if (deleteButtonTest === null) {
       let newButton = document.createElement('button');
-      newButton.textContent = "Delete";
+      newButton.textContent = 'Delete';
       newButton.setAttribute('class', 'delete-button');
       newButton.setAttribute('id', `button${i}`);
       cell5.appendChild(newButton);
-    } else {}
+    } else {
+    }
 
+    const readButtonTest = document.getElementById(`read${i}`); //test if element exists
 
-    const readButtonTest = document.getElementById(`read${i}`);   //test if element exists
-
-    if (readButtonTest === null) {   //testing if button already exists before creating new button
+    //testing if button already exists before creating new button
+    if (readButtonTest === null) {
       let newButton2 = document.createElement('button');
-      newButton2.textContent = "Read";
+      newButton2.textContent = 'Read';
       newButton2.setAttribute('class', 'read-button');
       newButton2.setAttribute('id', `read${i}`);
       cell4.appendChild(newButton2);
-    } else {}
+    } else {
+    }
   }
-  deleteBook();   //rerun so new 'delete' buttons would function
+  deleteBook(); //rerun so new 'delete' buttons would function
   readBook();
 }
 addBookToLibrary();
 
-
 function deleteBook() {
-
   for (let i = 0; i <= myLibrary.length - 1; i++) {
     const deleteButton = document.getElementById(`button${i}`);
     deleteButton.addEventListener('click', deleteBookId);
- 
+
     function deleteBookId() {
       console.log(`test${i}`);
       myLibrary.splice(i, 1);
@@ -144,14 +163,12 @@ function deleteBook() {
       console.log(myLibrary);
       console.log(myLibrary.length);
 
-      addBookToLibrary();   //execute to update the list
+      addBookToLibrary(); //execute to update the list
     }
   }
 }
 
-
 function readBook() {
-
   for (let i = 0; i <= myLibrary.length - 1; i++) {
     const readButton = document.getElementById(`read${i}`);
     readButton.addEventListener('click', readBookId);
@@ -159,11 +176,10 @@ function readBook() {
     function readBookId() {
       myLibrary[i].toggleRead();
 
-      addBookToLibrary();   //execute to update the list
+      addBookToLibrary(); //execute to update the list
     }
   }
 }
-
 
 function clearCells() {
   for (let i = 0; i <= 9; i++) {
@@ -177,12 +193,13 @@ function clearCells() {
     const cell3 = document.getElementById(`${cellId3}`);
     const cell4 = document.getElementById(`${cellId4}`);
     const cell5 = document.getElementById(`${cellId5}`);
-    cell1.textContent = "";
-    cell2.textContent = "";
-    cell3.textContent = "";
-    cell4.textContent = "";
+    cell1.textContent = '';
+    cell2.textContent = '';
+    cell3.textContent = '';
+    cell4.textContent = '';
     if (cell5.firstChild !== null) {
       cell5.firstChild.remove();
-    } else {}
+    } else {
+    }
   }
 }
